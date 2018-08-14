819caea80f1257fbf0918ef343f2a4a6410c2882
Mohanrau Francis <rasmatius@gmail.com>
2018-08-14 17:19:34 +0800

    remove unwated files

--- database/migrations/2018_01_22_173340_update_tables_rename_tables_name.php
+++ database/migrations/2018_01_22_173340_update_tables_rename_tables_name.php
@@ -13,13 +13,6 @@ class UpdateTablesRenameTablesName extends Migration
      */
     public function up()
     {
-        Schema::rename('sale_payment_mode_providers', 'sales_payments_modes_providers');
-
-        Schema::rename('sale_payment_mode_settings', 'sales_payments_modes_settings');
-
-        Schema::rename('sale_payments', 'sales_payments');
-
-        Schema::rename('sale_product_sizes', 'sales_products_sizes');
 
         Schema::rename('setting_keys', 'settings_keys');
     }
@@ -31,14 +24,6 @@ class UpdateTablesRenameTablesName extends Migration
      */
     public function down()
     {
-        Schema::rename('sales_payments_modes_providers', 'sale_payment_mode_providers');
-
-        Schema::rename('sales_payments_modes_settings', 'sale_payment_mode_settings');
-
-        Schema::rename('sales_payments', 'sale_payments');
-
-        Schema::rename('sales_products_sizes', 'sale_product_sizes');
-
         Schema::rename('settings_keys', 'setting_keys');
     }
 }
